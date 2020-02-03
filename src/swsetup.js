0477fca97f35e896069a550725b69006bf071a98
IgorVasiljevic <ivasiljevi1@etf.unsa.ba>
2020-02-03 11:38:50 +0100

    sw file path change

--- src/swsetup.js
+++ src/swsetup.js
@@ -1,6 +1,6 @@
 export function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../sw.js');
+        navigator.serviceWorker.register('./sw.js');
 
         let refreshing;
         navigator.serviceWorker.addEventListener('controllerchange', () => {
